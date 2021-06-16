25801276570b73ee343b84956b3c64619d9b7442
Kshatria <hts7808@gmail.com>
2021-06-16 21:59:35 +0300

    new module constants

--- js/data.js
+++ js/data.js
@@ -5,28 +5,26 @@ import {
   createArrayElements
 } from './util.js';
 
-const OFFERS = 10,
-  MINVALUE = 1,
-  MAXVALUE = 8,
-  MINROOMS = 1,
-  MAXROOMS = 8,
-  MINGUESTS = 1,
-  MAXGUESTS = 8,
-  MINLAT = 35.65000,
-  MAXLAT = 35.70000,
-  MINLNG = 139.70000,
-  MAXLNG = 139.80000,
-  ARCMINUTE = 5,
-  MINCOST = 1000,
-  MAXCOST = 1000000,
-  TYPES = ['palace', 'flat', 'house', 'bungalow', 'hotel'],
-  TIMECHECK = ['12:00', '13:00', '14:00'],
-  FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-  PHOTOS = [
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
-    'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg',
-  ];
+import {
+  OFFERS,
+  MINVALUE,
+  MAXVALUE,
+  MINROOMS,
+  MAXROOMS,
+  MINGUESTS,
+  MAXGUESTS,
+  MINLAT,
+  MAXLAT,
+  MINLNG,
+  MAXLNG,
+  ARCMINUTE,
+  MINCOST,
+  MAXCOST,
+  TYPES,
+  TIMECHECK,
+  FEATURES,
+  PHOTOS
+} from './constants.js';
 
 const location = {
   x: getRandomPositiveFloat(MINLAT, MAXLAT, ARCMINUTE),
