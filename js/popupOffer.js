01e3809866b5fbb8c57e80cf98317d1d8d901480
Kshatria <hts7808@gmail.com>
2021-06-26 15:15:45 +0300

    linter fix

--- js/popupOffer.js
+++ js/popupOffer.js
@@ -167,8 +167,7 @@ function createAvatar (data) {
   avatar.setAttribute('src', data.avatar);
 }
 
-const offers = createOffers()
-
+const offers = createOffers();
 
 function createPopupOffer (offer) {
   const offerData = offer.OFFER;
