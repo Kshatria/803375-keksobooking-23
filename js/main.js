deedd71b966a7a02d45bb6cee6b984188246295b
Kshatria <hts7808@gmail.com>
2021-06-26 15:12:03 +0300

    add form status function

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 import {createAllOffers} from './popupOffer.js';
+import {toggleFormStatus} from './formStatus.js';
 
 const map = document.querySelector('#map-canvas');
 
 createAllOffers(map);
+
+// true = enable form; false = disableform
+toggleFormStatus(false);
