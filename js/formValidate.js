87979a8a7f2ac9c983865239d7a5af999a9881d8
Kshatria <hts7808@gmail.com>
2021-06-27 14:55:34 +0300

    add function validate fields

---
+++ js/formValidate.js
@@ -0,0 +1,80 @@
+const type = document.querySelector('#type'),
+  price = document.querySelector('#price'),
+  timein = document.querySelector('#timein'),
+  timeout = document.querySelector('#timeout'),
+  rooms = document.querySelector('#room_number'),
+  capacity = document.querySelector('#capacity');
+
+function validatePriceOfType () {
+  switch (type.value) {
+    case 'bungalow':
+      price.setAttribute('min', '0');
+      price.setAttribute('placeholder', '0');
+      break;
+    case 'flat':
+      price.setAttribute('min', '1000');
+      price.setAttribute('placeholder', '1 000');
+      break;
+    case 'hotel':
+      price.setAttribute('min', '3000');
+      price.setAttribute('placeholder', '3 000');
+      break;
+    case 'house':
+      price.setAttribute('min', '5000');
+      price.setAttribute('placeholder', '5 000');
+      break;
+    case 'palace':
+      price.setAttribute('min', '10000');
+      price.setAttribute('placeholder', '10 000');
+      break;
+  }
+}
+
+function validatePrice () {
+  price.reportValidity();
+}
+
+function validateTime () {
+  timeout.value = timein.value;
+}
+
+function validateCapacityRooms () {
+  capacity.querySelectorAll('option').forEach((el) => {
+    el.setAttribute('disabled', 'disabled');
+  });
+
+  function getGuestsOptions (guests) {
+    return capacity.querySelector(`option[value="${guests}"]`).removeAttribute('disabled');
+  }
+
+  switch (rooms.value) {
+    case '1':
+      getGuestsOptions('1');
+      capacity.value = '1';
+      break;
+    case '2':
+      getGuestsOptions('1');
+      getGuestsOptions('2');
+      capacity.value = '2';
+      break;
+    case '3':
+      getGuestsOptions('1');
+      getGuestsOptions('2');
+      getGuestsOptions('3');
+      capacity.value = '3';
+      break;
+    case '100':
+      getGuestsOptions('0');
+      capacity.value = '0';
+      break;
+  }
+}
+
+validateCapacityRooms();
+
+export function validateFields () {
+  type.addEventListener('change', validatePriceOfType);
+  price.addEventListener('input', validatePrice);
+  timein.addEventListener('change', validateTime);
+  rooms.addEventListener('change', validateCapacityRooms);
+}
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import {createAllOffers} from './popupOffer.js';
+import {validateFields} from './formValidate.js';
 
 const map = document.querySelector('#map-canvas');
 
 createAllOffers(map);
+validateFields();
